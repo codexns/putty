2e6443c8557551d11f862051f77aeb037015bac9
wbond <will@wbond.net>
2015-10-29 01:50:04 -0400

    Explicit version info for tracking releases

--- version.h
+++ version.h
@@ -1,13 +1,3 @@
-/*
- * This header file provides the various versioning-related #defines
- * for a particular PuTTY build.
- *
- * When my automated build system does a full build, Buildscr
- * completely overwrites this file with information derived from the
- * circumstances and type of that build. The information _here_ is
- * default stuff used for local development runs of 'make'.
- */
-
-#define TEXTVER "Unidentified build"
-#define SSHVER "PuTTY-Unidentified-Local-Build"
-#define BINARY_VERSION 0,0,0,0
+#define TEXTVER "Release 0.65-codexns"
+#define SSHVER "PuTTY_Release_0.65_codexns"
+#define BINARY_VERSION 0,65,0,100
